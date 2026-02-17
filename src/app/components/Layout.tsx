15eaeba5e0c865ed6c10a298ed0b95ee747063bb
ramzi-bouzaiene <ramzibouzaiene00@gmail.com>
2026-02-17 12:46:37 +0100

    adding footer

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import { Sidebar } from './Sidebar'
 import { Header } from './Header'
+import { Footer } from './Footer'
 
 export function Layout() {
   return (
@@ -14,6 +15,7 @@ export function Layout() {
         <main className="flex-1 overflow-auto">
           <Outlet />
         </main>
+        <Footer />
       </div>
     </div>
   )
